12c92b17f0eb5720e7f64559ecd8e81fbc5d8f26
gallang <gallang1800016063@webmail.uad.ac.id>
2020-06-13 19:37:55 +0700

    modul 4

---
+++ biodata.php
@@ -0,0 +1,54 @@
+<html>
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 50%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+<h3>BIODATA ANDA</h3>
+<table>
+  <tr>
+    <th>Nama</th>
+    <td>
+    <?php
+    echo $_POST["NAMA"]."<br />";
+    ?>
+    </td>
+  </tr>
+  <tr>
+    <th>NIM</th>
+    <td>
+    <?php
+    echo $_POST["NIM"]."<br />";
+    ?>
+    </td>
+  </tr>
+  <tr>
+    <th>Jenis Kelamin</th>
+    <td>
+    <?php
+    echo $_POST["KELAMIN"]."<br />";
+    ?>
+    </td>
+  </tr>
+  <tr>
+    <th>Prodi</th>
+    <td>
+    <?php
+    echo $_POST["PRODI"]."<br />";
+    ?>
+    </td>
+  </tr>
+</table>
+</html>
